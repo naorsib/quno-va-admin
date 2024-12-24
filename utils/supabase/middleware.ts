5676128d10b47eb13f71e90bf66415a298087b94
Naor Gabriel Sibony <naor.sibony@gmail.com>
2024-12-24 01:09:07 +0100

    refactor(refactored needless middleware code): made more clear now

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -4,88 +4,71 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { appBase, routeConsts } from '@/consts/routing.const';
 
 export const updateSession = async (request: NextRequest) => {
-  // This `try/catch` block is only here for the interactive tutorial.
-  // Feel free to remove once you have Supabase connected.
-  try {
-    // Create an unmodified response
-    let response = NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
+  // Create an unmodified response
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll();
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
-            );
-            response = NextResponse.next({
-              request,
-            });
-            cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
-            );
-          },
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          response = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
         },
       },
-    );
+    },
+  );
 
-    // This will refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/server-side/nextjs
-    const user = await supabase.auth.getUser();
+  // This will refresh session if expired - required for Server Components
+  // https://supabase.com/docs/guides/auth/server-side/nextjs
+  const user = await supabase.auth.getUser();
 
-    // protected routes
-    if (request.nextUrl.pathname.startsWith(appBase)) {
-      if (user.error) {
-        return NextResponse.redirect(new URL(routeConsts.signIn, request.url));
-      }
-      if (!user.data.user.email_confirmed_at) {
-        return NextResponse.redirect(
-          new URL(
-            `${routeConsts.verifyEmail}?email=${user.data.user.email}`,
-            request.url,
-          ),
-        );
-      }
-
-      if (!user.data.user.phone_confirmed_at) {
-        console.log('request.url', request.url);
-        return NextResponse.redirect(
-          new URL(routeConsts.verifyOtp, request.url),
-        );
-      }
-      if (user.error) {
-        return NextResponse.redirect(new URL(routeConsts.signIn, request.url));
-      }
+  // protected routes
+  if (request.nextUrl.pathname.startsWith(appBase)) {
+    if (user.error) {
+      return NextResponse.redirect(new URL(routeConsts.signIn, request.url));
     }
-
-    if (request.nextUrl.pathname === '/' && !user.error) {
-      if (!user.data.user.phone_confirmed_at) {
-        return NextResponse.redirect(
-          new URL(routeConsts.verifyOtp, request.url),
-        );
-      }
+    if (!user.data.user.email_confirmed_at) {
       return NextResponse.redirect(
-        new URL(routeConsts.quincyDashboard, request.url),
+        new URL(
+          `${routeConsts.verifyEmail}?email=${user.data.user.email}`,
+          request.url,
+        ),
       );
     }
 
-    return response;
-  } catch {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
+    if (!user.data.user.phone_confirmed_at) {
+      console.log('request.url', request.url);
+      return NextResponse.redirect(new URL(routeConsts.verifyOtp, request.url));
+    }
+    if (user.error) {
+      return NextResponse.redirect(new URL(routeConsts.signIn, request.url));
+    }
   }
+
+  if (request.nextUrl.pathname === '/' && !user.error) {
+    if (!user.data.user.phone_confirmed_at) {
+      return NextResponse.redirect(new URL(routeConsts.verifyOtp, request.url));
+    }
+    return NextResponse.redirect(
+      new URL(routeConsts.quincyDashboard, request.url),
+    );
+  }
+
+  return response;
 };
