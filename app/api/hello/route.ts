9f1b4c58bc2fa93d7d851de9529a11a2686aa677
Naor Gabriel Sibony <naor.sibony@gmail.com>
2024-12-29 23:13:11 +0100

    feat: testing

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,25 +1,22 @@
 'use server';
 
 import { NextRequest, NextResponse } from 'next/server';
-import Retell from 'retell-sdk';
 
 import { createAdminClient } from '@/utils/supabase/admin-server';
 
 export async function GET(request: NextRequest) {
   const supabase_admin = await createAdminClient();
-  if (
-    !Retell.verify(
-      JSON.stringify(request.body),
-      process.env.RETELL_API_KEY as string,
-      request.headers.get('x-retell-signature') as string,
-    )
-  ) {
-    console.error('Invalid signature');
-    return;
-  }
-  console.log(Retell.PhoneNumber);
-  const searchParams = request.nextUrl.searchParams;
-  const phone = searchParams.get('phone');
+  //   if (
+  //     !Retell.verify(
+  //       JSON.stringify(request.body),
+  //       process.env.RETELL_API_KEY as string,
+  //       request.headers.get('x-retell-signature') as string,
+  //     )
+  //   ) {
+  //     console.error('Invalid signature');
+  //     return;
+  //   }
+  //   console.log(Retell.PhoneNumber);
 
-  return NextResponse.json({ message: `Hello, ${phone}!` });
+  return NextResponse.json({ message: `Hello, test!` });
 }
