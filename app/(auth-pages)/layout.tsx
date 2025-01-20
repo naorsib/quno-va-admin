015dd45ed69f87582cd72fc0eb3a1c45e67e345b
Naor Gabriel Sibony <naor.sibony@gmail.com>
2025-01-20 16:57:50 +0100

    chore(logo handling on auth pages): logo handling on auth pages

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,14 +1,24 @@
+import { getTranslations } from 'next-intl/server';
+
 import LogoSvgComponent from '@/components/react-svg-components/logo';
+import { StaticRouteLink } from '@/components/static-route-link';
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const t = await getTranslations(`Landing.header`);
+
   return (
     <div className="h-screen w-full overflow-auto bg-primary">
       <div className="flex flex-col items-start items-center gap-12 p-2 pt-9 md:px-6">
-        <LogoSvgComponent className="text-white lg:self-start" />
+        <StaticRouteLink routeTo="baseUrl">
+          <LogoSvgComponent
+            desc={t('logoDesc')}
+            className="text-white lg:self-start"
+          />
+        </StaticRouteLink>
         {children}
       </div>
     </div>
